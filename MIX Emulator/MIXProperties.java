389e9ff2c5646078c5af372f09bff0eac7e83fe8
Join2Learn <hoang.vt.152@gmail.com>
2015-10-25 23:51:35 +0700

    add Mix emulator

---
+++ MIX Emulator/MIXProperties.java
@@ -0,0 +1,11 @@
+public class MIXProperties
+{
+	private  char[] rA = new char[6];
+	private  char[] rX = new char [6];
+	private  char[] rJ = new char[2];
+	private  char[][] rI = new char[6][3];
+	//Overflow flag.....	
+	private  char flags;  /*[0-1][2] 0-1: 00 : equals - 01: less 10-greater */
+	private  char[] memoryBlock = new char[4000];
+	private  
+}
---
+++ MIX Emulator/MIX_Emulator.java
@@ -0,0 +1,22 @@
+public class MIX_Emulator
+{
+	public static void main(String[] args)
+	{
+		
+		
+		
+		//args[0]: Mode  --console mode: 0 script mode : 1
+		//args[1]: - script  name : = 1.
+		if (args[0] == 0)
+		{
+			
+		}
+		if (args[0] == 1 )
+		{
+			// Load file
+			// Parse line
+			// runfile
+			
+		}
+	}
+}
